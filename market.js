50a56189e19f0ab27b31cd6502542aafff76d640
dimasvoinboecvolk <158043890+dimasvoinboecvolk@users.noreply.github.com>
2024-05-08 13:00:12 +0300

    SDGbasdg

--- market.js
+++ market.js
@@ -7,13 +7,16 @@ let itemsDiv = document.getElementById("items");
 console.log(itemsDiv)
 
 if (itemsDiv) {
+    itemsArray.forEach((item) => {
+        itemsDiv.innerText += item
+    })
 
-    console.log(itemsDiv)
+    //console.log(itemsDiv)
 
-    console.log('Поле classList ', itemsDiv.classList)
-    console.log('Поле id ', itemsDiv.id)
-    console.log('Поле clientWidth ', itemsDiv.clientWidth)
-    console.log('Поле innerHTML ', itemsDiv.innerHTML)
+    //console.log('Поле classList ', itemsDiv.classList)
+    //console.log('Поле id ', itemsDiv.id)
+    //console.log('Поле clientWidth ', itemsDiv.clientWidth)
+    //console.log('Поле innerHTML ', itemsDiv.innerHTML)
 
     //itemsDiv.innerText = 'Програмно доданий текст'
     
