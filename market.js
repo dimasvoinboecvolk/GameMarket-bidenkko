41d819d8df7eb00e6910b6b51dfc8dfa4bc32e69
dimasvoinboecvolk <158043890+dimasvoinboecvolk@users.noreply.github.com>
2024-05-15 12:35:06 +0300

    ydygkedty

--- market.js
+++ market.js
@@ -1,6 +1,33 @@
 console.log('Перевірка підключеного файлу скриптів market.js')
 
-let itemsArray = ['газонокосорка','електричний тример','електро газонокосарка','акамуляторний оприскувач']
+//let itemsArray = ['газонокосорка','електричний тример','електро газонокосарка','акамуляторний оприскувач']
+//Визначення масиву об'єкті
+let itemsArray = [
+    {
+        firstName: "Віталій",
+        LastName: "Шатківський",
+        age: 43,
+        subject: "CS",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/"
+    },
+    {
+        firstName: "Наталія",
+        LastName: "Венцель",
+        age: 18,
+        subject: "Адміністратор",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/ventsel-n-v/"
+    },
+    {
+        firstName: "Наталія",
+        LastName: "Кучер",
+        age: 18,
+        subject: "Фізика",
+        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/kucher-nataliya-viktorivna-683x1024.jpg",
+        url: "https://lyceum.ztu.edu.ua/team/kucher-n-v/"
+    }
+]
 
 let itemsDiv = document.getElementById("items");
 
@@ -41,9 +68,16 @@ if (itemsDiv) {
     //itemsDiv.innerHTML += '<div class = item></div>'
     itemsArray.forEach((item,index) => {
         itemsDiv.innerHTML += 
-        `<div class = "item">
-        <h2>Товар №${index}</h2>
-        <p>${item}</p>
+        `
+        <div class = "item">
+        <h2>Вчитель №${index + 1} з ${itemsArray.length}</h2>
+        <p>${item.firstName} ${item.LastName}</p>
+        <p>предмет: ${item.subject}</p>
+        <p><img src = "${item.photo}" class = "item-image"></p>
+        <p><a href = "${item.url}" target = "_blank" class = "bonus price ">Профіль</a></p>
+        <p><a href = "mailto:${item.email}?subject=Питання&body=Доброго дня" target = "_blank"
+        class = "price ">Написати </a></p>
+        
         </div>`
     })
 
