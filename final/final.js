75bbdb5cfe2a87a649204799e2c644d8ea80bfc2
dimasvoinboecvolk <158043890+dimasvoinboecvolk@users.noreply.github.com>
2024-05-22 13:00:12 +0300

    arh

--- final/final.js
+++ final/final.js
@@ -4,30 +4,195 @@ console.log('Перевірка підключеного файлу скрипт
 //Визначення масиву об'єкті
 let itemsArray = [
     {
-        firstName: "Віталій",
-        LastName: "Шатківський",
-        age: 43,
-        subject: "CS",
-        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/03/dsc07815-683x1024.jpg",
-        url: "https://lyceum.ztu.edu.ua/team/shatkivskyy-v-m/"
-    },
-    {
-        firstName: "Наталія",
-        LastName: "Венцель",
-        age: 18,
-        subject: "Адміністратор",
-        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/ventsel-nataliya-vasylivna-683x1024.jpg",
-        url: "https://lyceum.ztu.edu.ua/team/ventsel-n-v/"
-    },
-    {
-        firstName: "Наталія",
-        LastName: "Кучер",
-        age: 18,
-        subject: "Фізика",
-        photo: "https://lyceum.ztu.edu.ua/wp-content/uploads/2023/04/kucher-nataliya-viktorivna-683x1024.jpg",
-        url: "https://lyceum.ztu.edu.ua/team/kucher-n-v/"
-    }
-]
+    email: "23b_bvo@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Башинська Вікторія",
+    img_author: "final/img1/Башинська.jpg",
+
+    project1Title: "Dudi 3",
+    project1Github: "https://github.com/ViktoriaBashinska/Phaser2stGame",
+    project1Website: "https://viktoriabashinska.github.io/Phaser2stGame/",
+    driveFolder: "https://drive.google.com/drive/folders/15v_c4DzKpW5323A4wYMV2Zj7AwRkSPUE?usp=sharing",
+    form: "https://forms.gle/jHMQsybKdMdUHXKA7",
+    project2Title: "Game Market",
+    project2Github: "https://github.com/ViktoriaBashinska/GameMarket_Bashynska",
+    project2Website: "https://viktoriabashinska.github.io/GameMarket_Bashynska/",
+    project2Market: "https://viktoriabashinska.github.io/GameMarket_Bashynska/market.html",
+    project2Final: "https://viktoriabashinska.github.io/GameMarket_Bashynska/final/final.html"
+  },
+  {
+    email: "23b_bmv@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Бовсуновський Максим",
+    project1Title: "дота 2",
+    project1Github: "https://github.com/maksbovs/Phaser2ndGame",
+    project1Website: "https://maksbovs.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1k4q30LAPEh8EpQpzamE_RYHICcYwkVG1?usp=sharing",
+    form: "https://forms.gle/G2WLe25CYPCQjowTA",
+    project2Github: "https://github.com/maksbovs/GameMarket_Bovsunoskiy",
+    project2Website: "https://maksbovs.github.io/GameMarket_Bovsunoskiy/",
+    project2Market: "https://maksbovs.github.io/GameMarket_Bovsunoskiy/market.html",
+    project2Final: "https://maksbovs.github.io/GameMarket_Bovsunoskiy/final/final.html"
+  },
+  {
+    email: "23b_vvi@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Волинець Владислав",
+    project1Title: "Пес Патрон на завданні",
+    project1Github: "https://github.com/volynets-vlad/Phaser2ndGame?authuser=0",
+    project1Website: "https://volynets-vlad.github.io/Phaser2ndGame/?authuser=0",
+    driveFolder: "https://drive.google.com/drive/folders/1g2bPWq8NGqzlBAlI8rMiOYNWBIuiCMRy?usp=classroom_web&authuser=0",
+    form: "https://forms.gle/5DGf6yDiZLAVGu7o9?authuser=0",
+    project2Github: "https://github.com/volynets-vlad/GameMarket_Volynets",
+    project2Website: "https://volynets-vlad.github.io/GameMarket_Volynets/",
+    project2Market: "https://volynets-vlad.github.io/GameMarket_Volynets/market.html",
+    project2Final: "https://volynets-vlad.github.io/GameMarket_Volynets/final/final.html"
+  },
+  {
+    email: "23b_gas@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Галетко Артем",
+    project1Title: "",
+    project1Github: "https://artem4ikgaletko.github.io/phaserSec2ndgame/?authuser=0",
+    project1Website: "https://artem4ikgaletko.github.io/phaserSec2ndgame/?authuser=0",
+    driveFolder: "https://drive.google.com/drive/folders/1HSnDCZfuIwp7RzFXJjG2IBUhC4Gx9eyL?usp=drive_link",
+    form: "https://forms.gle/cz6ZfWN7UFPUgwFC7?authuser=0",
+    project2Github: "https://github.com/Artem4ikGAletko/GameMarket_Haletko",
+    project2Website: "https://artem4ikgaletko.github.io/GameMarket_Haletko/",
+    project2Market: "",
+    project2Final: ""
+  },
+  {
+    email: "23b_gbs@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Гордійчук Богдан",
+    project1Title: "Zhytomur squad Snyok",
+    project1Github: "https://github.com/malintovi/Phaser2ndGame?authuser=0",
+    project1Website: "https://malintovi.github.io/Phaser2ndGame/?authuser=0",
+    driveFolder: "https://drive.google.com/drive/folders/1EYhBdclLisemO0-yRcLov0Qu3VMlvaVl?usp=sharing",
+    form: "https://forms.gle/VaDXCem7gVuEvumQ9",
+    project2Github: "https://github.com/malintovi/GameMarket_Hordiichuk",
+    project2Website: "https://malintovi.github.io/GameMarket_Hordiichuk/",
+    project2Market: "https://malintovi.github.io/GameMarket_Hordiichuk/market.html",
+    project2Final: "https://malintovi.github.io/GameMarket_Hordiichuk/final/final.html?authuser=0"
+  },
+  {
+    email: "23b_gai@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Гоцман Артем",
+    project1Title: "Run_Dude",
+    project1Github: "https://github.com/GOLDEN-ZTU/Phaser2ndGame",
+    project1Website: "https://golden-ztu.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/17jcUqrZBQaD6g0UtpsljRbQN-EcUqnT2",
+    form: "https://forms.gle/NTmMk1yqBNhaoXzj9",
+    project2Github: "https://github.com/GOLDEN-ZTU/GameMarket_Hotsman.git",
+    project2Website: "https://golden-ztu.github.io/GameMarket_Hotsman/",
+    project2Market: "https://golden-ztu.github.io/GameMarket_Hotsman/market.html",
+    project2Final: "https://golden-ztu.github.io/GameMarket_Hotsman/final/final.html"
+  },
+  {
+    email: "23b_god@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Гуменюк Олег",
+    project1Title: "",
+    project1Github: "https://github.com/OlegGumenuk",
+    project1Website: "https://oleggumenuk.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/10tRQ_WUvgqz0yGnm0J7WduMyziVFyoE5?usp=drive_link",
+    form: "https://forms.gle/iXKinscde4Cbqe6e7",
+    project2Github: "https://github.com/OlegGumenuk/GameMarket_Gumenuk",
+    project2Website: "https://oleggumenuk.github.io/GameMarket_Gumenuk/",
+    project2Market: "",
+    project2Final: ""
+  },
+  {
+    email: "23b_dis@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Дєдух Ілля",
+    project1Title: "kozak_battleground",
+    project1Github: "https://github.com/Dyeduh/Phaser2ndGame",
+    project1Website: "https://dyeduh.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1ms2y6ckh0DM3u-Li5iWtKVMketXVKQEy?usp=classroom_web&authuser=5",
+    form: "https://docs.google.com/forms/d/e/1FAIpQLSe7zr5KaMa5DfxzbKWN0Iz5FJs6ZoEX-4Vz5MB9erOUlKl0yQ/viewform?usp=sf_link&authuser=5",
+    project2Github: "https://github.com/Dyeduh/GameMarket_Dyeduh",
+    project2Website: "https://dyeduh.github.io/GameMarket_Dyeduh/",
+    project2Market: "https://dyeduh.github.io/GameMarket_Dyeduh/market.html?authuser=0",
+    project2Final: "https://dyeduh.github.io/GameMarket_Dyeduh/final/final.html?authuser=0"
+  },
+  {
+    email: "23b_dao@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Дідківський Андрій",
+    project1Title: "Баобаб",
+    project1Github: "https://github.com/AndriyDidkivskij/Phaser2ndGame",
+    project1Website: "https://andriydidkivskij.github.io/Phaser2ndGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1Q65jiZ9y4uJw_kcvvC3ma3fksBzR4Btp",
+    form: "https://forms.gle/WNDGSsahKLzHH7LW7",
+    project2Github: "https://github.com/AndriyDidkivskij/GameMarket_Didkivskij",
+    project2Website: "https://andriydidkivskij.github.io/GameMarket_Didkivskij/",
+    project2Market: "https://andriydidkivskij.github.io/GameMarket_Didkivskij/market.html?authuser=0",
+    project2Final: "https://andriydidkivskij.github.io/GameMarket_Didkivskij/final/final.html"
+  },
+  {
+    email: "23b_zbv@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Заєць Богдан",
+    project1Title: "",
+    project1Github: "https://github.com/niccer5/Phaser1stGame",
+    project1Website: "https://niccer5.github.io/Phaser1stGame/",
+    driveFolder: "https://drive.google.com/drive/folders/1NnmW02vDttgyJSTsOeB-hK3L-NuQjKr8",
+    form: "https://forms.gle/EcWrBsp6jhMA78Kd9",
+    project2Github: "https://github.com/niccer5/-GameMarket_Zayets",
+    project2Website: "https://niccer5.github.io/-GameMarket_Zayets/",
+    project2Market: "",
+    project2Final: ""
+  },
+  {
+    email: "23b_kav@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Кравчук Андрій",
+    project1Title: "",
+    project1Github: "https://github.com/yawty1",
+    project1Website: "https://yawty1.github.io/Phaser2ndGame1/",
+    driveFolder: "https://drive.google.com/drive/folders/1iTB53E5SwT1ds8PftFEwIUbkm5rPm6Ea?usp=drive_link",
+    form: "https://forms.gle/1n5FQ3NmUX6fWiiB6",
+    project2Github: "https://github.com/yawty1/GameMarket_Kravchuk",
+    project2Website: "https://yawty1.github.io/GameMarket_Kravchuk/",
+    project2Market: "",
+    project2Final: ""
+  },
+  {
+    email: "23b_kmv@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Кучер Максим",
+    project1Title: "Jfr",
+    project1Github: "https://github.com/KucherMaks/Phaser2ndGame?authuser=4&hl=ru",
+    project1Website: "https://kuchermaks.github.io/Phaser2ndGame/?authuser=4&hl=ru",
+    driveFolder: "https://drive.google.com/drive/folders/1OGZCkFefm150MhQfef1UEbgJImuC4diT?usp=sharing",
+    form: "https://forms.gle/yaJ2EMs6hNbRpTTg9",
+    project2Github: "https://github.com/KucherMaks/GameMarket_Kucher",
+    project2Website: "https://kuchermaks.github.io/GameMarket_Kucher/",
+    project2Market: "https://kuchermaks.github.io/GameMarket_Kucher/market.html",
+    project2Final: "https://kuchermaks.github.io/GameMarket_Kucher/final/final.html"
+  },
+  {
+    email: "23b_lkm@liceum.ztu.edu.ua",
+    class: "10-Б 1",
+    name: "Лавренко Каріна",
+    project1Title: "Dudi 2",
+    project1Github: "https://github.com/KarinaLavrenko/Phaser2stGame?authuser=0",
+    project1Website: "https://karinalavrenko.github.io/Phaser2stGame/?authuser=0",
+    driveFolder: "https://drive.google.com/drive/folders/1T8N8q-PBfSBOrRVSNBdqb4bULfN1ZaE9?usp=classroom_web&authuser=0",
+    form: "https://forms.gle/XbBbwwGJ8kJ5tTya8?authuser=0",
+    project2Github: "https://github.com/KarinaLavrenko/GameMarket_Lavrenko?authuser=0",
+    project2Website: "https://karinalavrenko.github.io/GameMarket_Lavrenko/",
+    project2Market: "https://karinalavrenko.github.io/GameMarket_Lavrenko/market.html?authuser=0",
+    project2Final: "https://karinalavrenko.github.io/GameMarket_Lavrenko/final/final.html?authuser=0"
+  }
+];
+
+    
+
+
 
 let itemsDiv = document.getElementById("items");
 
@@ -35,72 +200,45 @@ console.log(itemsDiv)
 
 if (itemsDiv) {
     itemsArray.forEach((item) => {
-        itemsDiv.innerText += item
-    })
+        itemsDiv.innerText += `
+        <!--  Блок окремого товару -->
+        <div class="item">
+        <div class="item-title">${item.title}</div>
 
-    //console.log(itemsDiv)
-
-    //console.log('Поле classList ', itemsDiv.classList)
-    //console.log('Поле id ', itemsDiv.id)
-    //console.log('Поле clientWidth ', itemsDiv.clientWidth)
-    //console.log('Поле innerHTML ', itemsDiv.innerHTML)
-
-    //itemsDiv.innerText = 'Програмно доданий текст'
-    
-    //itemsDiv.innerText = 'Перший Програмно доданий текст'
-    //itemsDiv.innerText += 'Другий Програмно доданий текст'
-
-    //itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
-
-
-} else {
-
-    console.log('Блок товарів не найдено')
-}
-
-if (itemsDiv) {
-
-    console.log(itemsDiv)
-
-    //itemsDiv.innerHTML += '<div class = item></div>'
-    //itemsDiv.innerHTML += '<div class = item></div>'
-   // itemsDiv.innerHTML += '<div class = item></div>'
-    //itemsDiv.innerHTML += '<div class = item></div>'
-    itemsArray.forEach((item,index) => {
-        itemsDiv.innerHTML += 
-        `
-        <div class = "item">
-        <h2>Вчитель №${index + 1} з ${itemsArray.length}</h2>
-        <p>${item.firstName} ${item.LastName}</p>
-        <p>предмет: ${item.subject}</p>
-        <p><img src = "${item.photo}" class = "item-image"></p>
-        <p><a href = "${item.url}" target = "_blank" class = "bonus price ">Профіль</a></p>
-        <p><a href = "mailto:${item.email}?subject=Питання&body=Доброго дня" target = "_blank"
-        class = "price ">Написати </a></p>
+        <!-- Блок зображення товару -->
+        <div class="item-image">
+        <img src="${item.image}" class = "item-iamge">
+        </div>
         
-        </div>`
+        <!-- Блок з оплатою частинами -->
+        <div class="parts-pay">
+        <div> class ="parts-pay">
+        <div><img src="img/bg.png" alt="">${item.mono}</div>
+        <div><img src="img/prom.png" alt="">${item.pb}</div>
+        </div>
+
+        <!-- Блок Ціни -->
+        <div class="prise">
+        
+        <div><span>${item.prise_standart} </span><sup>грн</sup></div>
+        <div><span>${item.prise_discount} </span><sup>грн</sup></div>
+        </div>
+
+        <!-- Блок бонусної ціни -->
+        <div class="prise bonus">
+        <div> ціна за купоном </div>
+        <div><span>${item.prise_coupon} </span><sup>грн</sup></div>
+        </div>
+        </div>
+        
+  
+
+        
+        `
     })
 
-} else {
 
-    console.log('Блок товарів не знайдено')
-}
+    } else {
 
-
-
-
-console.log(itemsArray)
-
-//itemsArray = itemsArray.sort().forEach((item) => {
-    //console.log( item)
-//})
-
-itemsArray.sort().forEach((item, index) => {
-    console.log(index + '-й елемент: ', item)})
-
-
-
-for (let i = 0; i < itemsArray.length; i++) {
-    console.log(i + '-й елемент: ' ,itemsArray[i])
-}
-
+        console.log('Блок товарів не найдено')
+    }
